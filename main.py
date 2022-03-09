04c03c82091b66baed0c3d2ed064b930ba9a5378
junho <kesu9251@naver.com>
2022-03-09 18:37:50 +0900

    옵션으로 주식과 코인 두개 돌릴 수 있도록 추가

--- main.py
+++ main.py
@@ -3,49 +3,57 @@ from datetime import datetime
 from models.RSITrade import RSIAlgorithm
 from models.VolumeChange import VolumeChange
 from utils.telegram_bot import TelegramBot
+import argparse
 
 if __name__ == '__main__':
-    data_handler = DataHandler(False)
-    data_handler.download_stock_info()
-    data_handler.get_stocks_list()
-    today_date = datetime.now().strftime('%Y-%m-%d')
-    today_stock_data = list()
-    i = 0
-    while 1:
-        if data_handler.set_next_stock(start_time=today_date) is False:
-            break
-        data = data_handler.next_data()
-        if data is not 0:
-            today_stock_data.append(data)
-        i += 1
-        if i % 100 is 0:
-            print(i)
-    algorithm = RSIAlgorithm()
-    algorithm2 = VolumeChange()
-    rsi_stocks = algorithm.catch_stocks(today_stock_data)
-    volume_stocks = algorithm2.catch_stocks(today_stock_data)
-    with open('outputs/'+today_date+'_output.txt','w') as f:
-        for stock in rsi_stocks:
-            value = [str(i) for i in stock.values()]
-            f.writelines("_".join(value))
-            f.write('\n')
-        f.write('====================================================')
-        for stock in volume_stocks:
-            value = [str(i) for i in stock.values()]
-            f.writelines("_".join(value))
-            f.write('\n')
-
-    print('done')
 
     tgBot = TelegramBot()
 
-    result = str(rsi_stocks)
-    for i in range(0,len(result),1000):
-        tgBot.sendmsg(result[i:i+1000])
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--stock', action='store_true')
+    parser.add_argument('--coin', action='store_true')
+    args = parser.parse_args()
 
-    result = str(volume_stocks)
-    for i in range(0,len(result),1000):
-        tgBot.sendmsg(result[i:i+1000])
+    if args.stock is True:
+        data_handler = DataHandler(False)
+        data_handler.download_stock_info()
+        data_handler.get_stocks_list()
+        today_date = datetime.now().strftime('%Y-%m-%d')
+        today_stock_data = list()
+        i = 0
+        while 1:
+            if data_handler.set_next_stock(start_time=today_date) is False:
+                break
+            data = data_handler.next_data()
+            if data is not 0:
+                today_stock_data.append(data)
+            i += 1
+            if i % 100 is 0:
+                print(i)
+        algorithm = RSIAlgorithm()
+        algorithm2 = VolumeChange()
+        rsi_stocks = algorithm.catch_stocks(today_stock_data)
+        volume_stocks = algorithm2.catch_stocks(today_stock_data)
+        with open('outputs/'+today_date+'_output.txt','w') as f:
+            for stock in rsi_stocks:
+                value = [str(i) for i in stock.values()]
+                f.writelines("_".join(value))
+                f.write('\n')
+            f.write('====================================================')
+            for stock in volume_stocks:
+                value = [str(i) for i in stock.values()]
+                f.writelines("_".join(value))
+                f.write('\n')
+
+        print('done')
+
+        result = str(rsi_stocks)
+        for i in range(0,len(result),1000):
+            tgBot.sendmsg(result[i:i+1000])
+
+        result = str(volume_stocks)
+        for i in range(0,len(result),1000):
+            tgBot.sendmsg(result[i:i+1000])
 
 '''
     # # #주식데이터로 보조지표를 만들어 낸다.
